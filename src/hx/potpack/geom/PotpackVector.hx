26379de95e22710ee7aadc2925e5bb90d5e36845
Rainy <939029835@qq.com>
2026-01-29 17:32:12 +0800

    Add cs java eval condition.

--- src/hx/potpack/geom/PotpackVector.hx
+++ src/hx/potpack/geom/PotpackVector.hx
@@ -1,6 +1,6 @@
 package hx.potpack.geom;
 
-#if (neko && openfl)
+#if ((neko || cs || java || eval) && openfl)
 typedef PotpackVector<T> = openfl.Vector<T>;
 #else
 typedef PotpackVector<T> = haxe.ds.Vector<T>;
